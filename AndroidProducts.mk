da7c3082817c1200ee9ff75181a7628e83c5d40a
Kamaldeen Jimoh <jimohkamal446@gmail.com>
2023-09-18 11:52:19 +0100

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's omni device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_KE5.mk
+
+COMMON_LUNCH_CHOICES := \
+     omni_KE5-eng
